aa2898094d785543eab01d40cc8c8fe11e50be5c
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2022-09-21 06:42:39 +0100

    Update LocalWeather.jsx

--- src/components/LocalWeather.jsx
+++ src/components/LocalWeather.jsx
@@ -5,6 +5,11 @@ function LocalWeather() {
   return (
 <span>
   <App />
+     <div className="p-3">
+      <h1>This app's source code:</h1>
+    </div>
+    
+    <a href="https://github.com/DavittBarry/Country-Weather-API-search"> GitHub repository</a>
 </span> 
 );
 }
