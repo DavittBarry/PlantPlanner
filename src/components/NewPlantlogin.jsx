1be1ade1965a9dbb645273f1f103efcf4e407e1d
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2023-01-08 15:52:06 +0200

    Fixed plants and added preview

--- src/components/NewPlantlogin.jsx
+++ src/components/NewPlantlogin.jsx
@@ -1,60 +1,61 @@
-import React from "react";
-const { getInitationTime } = require('../routes/timer/timer.js')
+import React, { useState } from "react";
+import { getInitationTime } from '../routes/timer/timer';
 
-function NewPlant() {
-  return (
-    <div className="introduction NewPlant slideshow-container">
-      <div>
-          <div className="create-user">
-            <div className="d-flex justify-content-center">
-              <form className="NewUser">
+const NewPlant = () => {
+const [plantName, setPlantName] = useState('');
+const [additionalInfo, setAdditionalInfo] = useState('');
+const [wateringCycle, setWateringCycle] = useState('');
+const [watered, setWatered] = useState(false);
+const [previewVisible, setPreviewVisible] = useState(false);
 
-                <div >
-                  <h1>New Plant</h1>
-                </div>
-                <div >
-                  <div className="p-2">
-                    <label for="Plantname">Plant name:</label>
-                    <div >
-                    <input type="text" id="Plantname" name="Plantname" placeholder="Your plant's name..."></input>
-                    </div>
-                  </div>
+const handleSubmit = (e) => {
+e.preventDefault();
+getInitationTime();
+setPreviewVisible(true);
+}
 
-                </div>
-                <div className="rp-2 p-2">
-                  <label for="info">Additional info:</label>
-                  <br/>
-                  <textarea id="subject" name="subject" placeholder="Write something..."></textarea>
-                    
-                </div>
-
-                <div className="p-2" >
-                  <div>  
-                    <label for="Watering cycle">Watering cycle:</label>
-                    <br/>
-                    <select select id="country" name="country">
-                    <option value="week">Once a week</option>
-                    <option value="2week">Every two weeks</option>
-                    <option value="month">Once a month</option>
-                    </select>
-                  </div>
-                </div>
-
-                <div className="p-2">
-                  <label for="plant-watered">Plant has been watered:</label>
-                  <br/>
-                  <input type="checkbox" id="plant-watered"/>
-                  <span id="js-initiation-time"></span>
-                </div>      
-                <div className="row justify-content-center">
-                <input type="submit" value="Submit" onClick={getInitationTime}></input>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-  );
+return (
+<div className=" NewPlant slideshow-container" style={{ backgroundColor: '#E6F8B2' }}>
+{previewVisible && (
+<div className="card container" style={{ backgroundColor: '#E6F8B2' }}>
+<h3 className="card-body">Preview:</h3>
+<div className="row card-body" >
+<h3 className="card-title">{plantName}</h3>
+<p className="card-text">Additional info: {additionalInfo}</p>
+<p className="card-text">Watering cycle: {wateringCycle}</p>
+<p className="card-text">Watered: {watered ? 'Yes' : 'No'}</p>
+</div>
+</div>
+)}
+<form className="card NewUser" onSubmit={handleSubmit} style={{ backgroundColor: '#E6F8B2' }}>
+<div className="card-body" style={{ backgroundColor: '#E6F8B2' }}>
+<h3 className="card-title" >New Plant</h3>
+<div className="form-group">
+<label for="plantName">Plant name:</label>
+<input type="text" className="form-control" id="plantName" name="plantName" placeholder="Your plant's name..." onChange={(e) => setPlantName(e.target.value)} />
+</div>
+<div className="form-group">
+<label for="additionalInfo">Additional info:</label>
+<textarea className="form-control" id="additionalInfo" name="additionalInfo" placeholder="Write something..." onChange={(e) => setAdditionalInfo(e.target.value)} />
+</div>
+<div className="form-group">
+<label for="wateringCycle">Watering cycle:</label>
+<select className="form-control" id="wateringCycle" name="wateringCycle" onChange={(e) => setWateringCycle(e.target.value)}>
+<option value="week">Once a week</option>
+<option value="2week">Every two weeks</option>
+<option value="month">Once a month</option>
+</select>
+</div>
+<div className="form-group form-check">
+<input type="checkbox" className="form-check-input" id="watered" onChange={(e) => setWatered(e.target.checked)} />
+<label className="form-check-label" for="watered">Plant has been watered:</label>
+<span id="js-initiation-time"></span>
+</div>
+<button type="submit" className="btn btn-success">Submit</button>
+</div>
+</form>
+</div>
+);
 }
 
 export default NewPlant;
