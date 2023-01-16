0975ded460acb0a62b8ac91a286d06e8a889b4d5
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2023-01-16 17:04:11 +0200

    Fixed user plant collection view and state

--- src/components/Plants/Posts.jsx
+++ src/components/Plants/Posts.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Post.css';
 
 function Posts() {
   return (
-    <div className="posts">
+    <div className="posts p-3">
       <div class="PlantPrev">
         <Link to="/Plants/this-is-a-post-title">
           <div class="row align-items-center">
-          
-              <h1 class="font-weight-light">Kasvikortti 1</h1>
-              <p>
-                Tähän "sneak peak" tekstiä
-              </p>
-            </div>
+
+            <h1 class="font-weight-light">Plant card 1</h1>
+            <p>
+              Sneak peak text.
+            </p>
+          </div>
 
         </Link>
       </div>
       <div class="PlantPrev">
         <Link to="/Plants/this-is-a-post-title">
           <div class="row align-items-center">
-              <h1 class="font-weight-light">Kasvikortti 2</h1>
-              <p>
-                Tähän "sneak peak" tekstiä
-              </p>
-            </div>
+            <h1 class="font-weight-light">Plant card 2</h1>
+            <p>
+              Sneak peak text.
+            </p>
+          </div>
         </Link>
       </div>
     </div>
