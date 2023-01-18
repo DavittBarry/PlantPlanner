3a1ea18fdedb13a88e8f095445f48b97755c7882
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2023-01-18 14:47:32 +0200

    Plant preview Watering and Fertilizing improved

--- src/components/Plants/Post.jsx
+++ src/components/Plants/Post.jsx
@@ -4,8 +4,8 @@ import './Post.css';
 
 const Post = () => {
   const { postSlug } = useParams();
-  const [wateringValue, setWateringValue] = useState(0);
-  const [fertilizingValue, setFertilizingValue] = useState(0);
+  const [wateringValue, setWateringValue] = useState(1);
+  const [fertilizingValue, setFertilizingValue] = useState(1);
 
   useEffect(() => {
     // Fetch post using the postSlug
@@ -20,45 +20,46 @@ const Post = () => {
   }
 
   const fertilizingTimeScales = {
-    1: "Once a month",
-    2: "Once every 2 weeks",
-    3: "Once a week",
-    4: "Every 3 days",
-    5: "Every day"
+    1: "Every month",
+    2: "Every 2 months",
+    3: "Once every 6 month",
+    4: "Once a year",
+    5: "Once every two years"
   }
 
 
   return (
     <div className="post-container rounded" >
       <div className="plant-card watering-card rounded" style={{ backgroundColor: "#70917" }}>
-        <div className="info-card " >
-          <h2 >Rose</h2>
-          <div className="info-card-text p-1 ">
+        <div className="info-card p-3" >
+          <h5 className="text-center" >Rose</h5>
+          <div className="info-card-text p-3 plant-card2 rounded">
             <p>The rose is a type of flowering shrub. Its name comes from the Latin word Rosa. There are over three hundred species and tens of thousands of cultivars. The flowers of the rose grow in many different colors, from the well-known red rose or yellow rose and sometimes white or purple rose.</p>
           </div>
         </div>
         <img src='https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="plantImg" className="plant-img" />
         <div className="plant-info ">
-          <div className="watering-card card-spacing" style={{ backgroundColor: "#70917" }}>
-            <h4>Watering</h4>
-            <div className="watering-slider card-spacing">
+          <div className="plant-card2 card-spacing rounded " style={{ backgroundColor: "#70917" }}>
+            <h3>Watering</h3>
+            <div className="p-3 card-spacing">
               <input type="range" min="1" max="5" value={wateringValue} onChange={(e) => setWateringValue(e.target.value)} />
             </div>
-            <div className="watering-status">
-              <p>Current watering level: </p>
+            <div className="watering-status test-dark">
+              <p><b>Current watering level: </b></p>
               <p> {wateringTimeScales[wateringValue]}</p>
-              <p>Last watered: 17.3.2022</p>
+              <p><b>Last watered:</b></p>
+              <p>17.3.2022</p>
             </div>
           </div>
-          <div className="fertilizing-card card-spacing" style={{ backgroundColor: "#70917" }}>
-            <h4>Fertilizing</h4>
-            <div className="fertilizing-slider card-spacing">
+          <div className="plant-card2 card-spacing rounded" style={{ backgroundColor: "#70917" }}>
+            <h3>Fertilizing</h3>
+            <div className="p-3 card-spacing">
               <input type="range" min="1" max="5" value={fertilizingValue} onChange={(e) => setFertilizingValue(e.target.value)} />
             </div>
             <div className="fertilizing-status">
-              <p>Current fertilizing level: </p>
+              <p><b>Current fertilizing level: </b></p>
               <p> {fertilizingTimeScales[fertilizingValue]}</p>
-              <p>Last fertilized: 1.3.2021</p>
+              <p><b>Last fertilized:</b></p><p> 1.3.2021</p>
             </div>
           </div>
         </div>
